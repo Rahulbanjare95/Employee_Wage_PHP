8f6c6774ef284bd50cef07c8ac837cfcdb8c0f9f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 11:48:31 +0530

    Refactored: Code to use swutch case instead of if else

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -4,24 +4,23 @@ $IS_PART_TIME = 1;
 $IS_FULL_TIME = 2;
 $EMP_RATE_PER_HR = 20;
 $empHrs = 0;
-$checkEmployee = rand(0,2);
-if ($checkEmployee == $IS_PART_TIME) {
-    $empHrs = 8;
-    echo "Employee is present Part time ";
-    $empWage = $empHrs * $EMP_RATE_PER_HR;
+$empWage = 0;
+$checkEmployee = mt_rand(0,2);
+switch ($checkEmployee) {
+    case $IS_PART_TIME:
+        $empHrs = 8;
+        echo " Part Time ";
+        break;
+    case $IS_FULL_TIME:
+        $empHrs = 12;
+        echo " Full Time ";
+        break;    
+    default: 
+        $empHrs = 0; 
+        echo " Absent ";
+        break;
 }
-elseif ($checkEmployee == $IS_FULL_TIME) 
-{
-    $empHrs = 12;
-    echo "Employee is present Full time ";
-    $empWage = $empHrs * $EMP_RATE_PER_HR;
-}
-else{
-    $empHrs = 0;
-    echo "Employee is absent";
-    $empWage = $empHrs * $EMP_RATE_PER_HR;
-}
-
-echo " <br> Employee wage is $empWage ";
+$empWage = $empHrs * $EMP_RATE_PER_HR;
+echo " <br> Employee Wage is = $empWage";
 
 ?>
