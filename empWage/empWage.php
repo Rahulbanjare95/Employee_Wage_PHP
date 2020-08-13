71618ed9398745911bfb6447b12b18fc9655386a
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 12:43:37 +0530

    Refactored: Code to use while loop and check the total working hours and working days are within allowed range

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -3,31 +3,31 @@
 $IS_PART_TIME = 1;
 $IS_FULL_TIME = 2;
 $EMP_RATE_PER_HR = 20;
-$TOTAL_WORKING_DAYS = 20;
+$MAX_WORKING_DAYS = 20;
+$MAX_HOURS_IN_MONTH = 100;
 $monthlyWage = 0;
 $empHrs = 0;
 $empWage = 0;
+$totalWorkingDays = 0;
+$totalWorkingHours = 0;
 
-for ($i=1; $i <=$TOTAL_WORKING_DAYS ; $i++) { 
+while ($totalWorkingDays <= $MAX_WORKING_DAYS && $totalWorkingHours <= $MAX_HOURS_IN_MONTH) {
+    $totalWorkingDays++;
     $checkEmployee = mt_rand(0,2);
     switch ($checkEmployee) {
         case $IS_PART_TIME:
             $empHrs = 8;
-            echo " day $i  = Part Time <br>";
             break;
         case $IS_FULL_TIME:
             $empHrs = 12;
-            echo " day $i  = Full Time <br>";
             break;    
         default: 
             $empHrs = 0; 
-            echo " day $i  = Absent <br>";
             break;    
 }
+$totalWorkingHours += $empHrs;
 $empWage = $empHrs * $EMP_RATE_PER_HR;
 $monthlyWage += $empWage;
 }
-
 echo " <br> Employee Wage for month is = $monthlyWage";
-
 ?>
