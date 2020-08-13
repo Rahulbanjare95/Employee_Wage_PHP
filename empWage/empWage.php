e0a881342095464a113f4b4b9a57579116756d68
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 10:04:54 +0530

    Refactored: Code to calculate wage according to Part time and Full time work hours

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -1,12 +1,19 @@
 <?php
 
-$IS_PRESENT = 1;
+$IS_PART_TIME = 1;
+$IS_FULL_TIME = 2;
 $EMP_RATE_PER_HR = 20;
 $empHrs = 0;
-$checkEmployee = rand(0,1);
-if ($checkEmployee == $IS_PRESENT) {
+$checkEmployee = rand(0,2);
+if ($checkEmployee == $IS_PART_TIME) {
     $empHrs = 8;
-    echo "Employee is present ";
+    echo "Employee is present Part time ";
+    $empWage = $empHrs * $EMP_RATE_PER_HR;
+}
+elseif ($checkEmployee == $IS_FULL_TIME) 
+{
+    $empHrs = 12;
+    echo "Employee is present Full time ";
     $empWage = $empHrs * $EMP_RATE_PER_HR;
 }
 else{
