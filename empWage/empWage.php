3e8b0ea8d63b96f77e1b1f03fbd9677a4c44bfae
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 13:45:19 +0530

    Refactored: Code to calculate employee hours using fucntion

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -5,16 +5,15 @@ $IS_FULL_TIME = 2;
 $EMP_RATE_PER_HR = 20;
 $MAX_WORKING_DAYS = 20;
 $MAX_HOURS_IN_MONTH = 100;
-
 $monthlyWage = 0;
 $empHrs = 0;
 $empWage = 0;
 $totalWorkingDays = 0;
 $totalWorkingHours = 0;
 
-while ($totalWorkingDays <= $MAX_WORKING_DAYS && $totalWorkingHours <= $MAX_HOURS_IN_MONTH) {
-    $totalWorkingDays++;
-    $checkEmployee = mt_rand(0,2);
+function getWorkingHours($checkEmployee)
+{ 
+    global $IS_FULL_TIME, $IS_PART_TIME;
     switch ($checkEmployee) {
         case $IS_PART_TIME:
             $empHrs = 8;
@@ -25,11 +24,20 @@ while ($totalWorkingDays <= $MAX_WORKING_DAYS && $totalWorkingHours <= $MAX_HOUR
         default: 
             $empHrs = 0; 
             break;  
+    }
+    return $empHrs;
+}
+function calculateDailyWage($empHrs){
+   global $EMP_RATE_PER_HR;
+    return $empHrs* $EMP_RATE_PER_HR;
+}
 
-}
+while ($totalWorkingDays <= $MAX_WORKING_DAYS && $totalWorkingHours <= $MAX_HOURS_IN_MONTH) {
+    $totalWorkingDays++;    
+$checkEmployee = mt_rand(0,2);
+$empHrs = getWorkingHours($checkEmployee);
 $totalWorkingHours += $empHrs;
-$empWage = $empHrs * $EMP_RATE_PER_HR;
-$monthlyWage += $empWage;
 }
+$monthlyWage = $totalWorkingHours * $EMP_RATE_PER_HR;
 echo " <br> Employee Wage for month is = $monthlyWage";
 ?>
