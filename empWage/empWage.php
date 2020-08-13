cb0167d91c2f269ad4b42f39c3f9e3044f9e30ac
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 11:58:23 +0530

    Refactored: Code to give monthly wage and printed day wise wokring hours

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -3,24 +3,31 @@
 $IS_PART_TIME = 1;
 $IS_FULL_TIME = 2;
 $EMP_RATE_PER_HR = 20;
+$TOTAL_WORKING_DAYS = 20;
+$monthlyWage = 0;
 $empHrs = 0;
 $empWage = 0;
-$checkEmployee = mt_rand(0,2);
-switch ($checkEmployee) {
-    case $IS_PART_TIME:
-        $empHrs = 8;
-        echo " Part Time ";
-        break;
-    case $IS_FULL_TIME:
-        $empHrs = 12;
-        echo " Full Time ";
-        break;    
-    default: 
-        $empHrs = 0; 
-        echo " Absent ";
-        break;
+
+for ($i=1; $i <=$TOTAL_WORKING_DAYS ; $i++) { 
+    $checkEmployee = mt_rand(0,2);
+    switch ($checkEmployee) {
+        case $IS_PART_TIME:
+            $empHrs = 8;
+            echo " day $i  = Part Time <br>";
+            break;
+        case $IS_FULL_TIME:
+            $empHrs = 12;
+            echo " day $i  = Full Time <br>";
+            break;    
+        default: 
+            $empHrs = 0; 
+            echo " day $i  = Absent <br>";
+            break;    
 }
 $empWage = $empHrs * $EMP_RATE_PER_HR;
-echo " <br> Employee Wage is = $empWage";
+$monthlyWage += $empWage;
+}
+
+echo " <br> Employee Wage for month is = $monthlyWage";
 
 ?>
