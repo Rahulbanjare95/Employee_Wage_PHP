89749d41c1424db7788f668dcb2dc84b6d49159f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 12:56:21 +0530

    Refactored: Code to calculate daily wage till maximum hours is reached

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -5,6 +5,7 @@ $IS_FULL_TIME = 2;
 $EMP_RATE_PER_HR = 20;
 $MAX_WORKING_DAYS = 20;
 $MAX_HOURS_IN_MONTH = 100;
+
 $monthlyWage = 0;
 $empHrs = 0;
 $empWage = 0;
@@ -23,7 +24,8 @@ while ($totalWorkingDays <= $MAX_WORKING_DAYS && $totalWorkingHours <= $MAX_HOUR
             break;    
         default: 
             $empHrs = 0; 
-            break;    
+            break;  
+
 }
 $totalWorkingHours += $empHrs;
 $empWage = $empHrs * $EMP_RATE_PER_HR;
