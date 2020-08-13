c47c20468c86fe3d4f5281458c76939edfdd5f2d
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-08-13 09:54:47 +0530

    Refactored: Code to calculate employee wage when employee is present

--- empWage/empWage.php
+++ empWage/empWage.php
@@ -1,13 +1,20 @@
 <?php
 
 $IS_PRESENT = 1;
-
+$EMP_RATE_PER_HR = 20;
+$empHrs = 0;
 $checkEmployee = rand(0,1);
 if ($checkEmployee == $IS_PRESENT) {
-    echo "Employee is Present";    
+    $empHrs = 8;
+    echo "Employee is present ";
+    $empWage = $empHrs * $EMP_RATE_PER_HR;
 }
-else
-    echo "Employee is Absent";
+else{
+    $empHrs = 0;
+    echo "Employee is absent";
+    $empWage = $empHrs * $EMP_RATE_PER_HR;
+}
 
+echo " <br> Employee wage is $empWage ";
 
 ?>
